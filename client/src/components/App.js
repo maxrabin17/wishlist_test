cfbede8f70568028806dc2a8b8d565e277a5a512
maxrabin <max.rabin17@gmail.com>
2021-08-12 16:08:11 -0400

    test

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import { Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Button variant="primary">Primary</Button>
+    </div>
+  );
+}
+
+export default App;
